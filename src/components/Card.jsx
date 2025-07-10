73f6455e90389df126f3a0eaf3e62cf796afbcb6
Animes <animesbarman101@gmail.com>
2025-07-10 12:36:29 +0600

    setup

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './card.css'
+
+export default function Card() {
+  return (
+    <div className='card'>
+        <h1>Card Title</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, alias autem possimus est optio recusandae nihil porro beatae laborum. Quos.
+
+        </p>
+        <button>View</button>
+
+
+    </div>
+  )
+}
